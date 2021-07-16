f720a44bac5a1f067e934fb9a5261b5b1e04bd89
RichVagos <87157952+RichVagos@users.noreply.github.com>
2021-07-16 18:10:03 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+ apiKey: "AIzaSyChPty4gf7f-zujXSST5EUJd37AhoCIymA",
+    authDomain: "chat41r-45cb4.firebaseapp.com",
+    projectId: "chat41r-45cb4",
+    storageBucket: "chat41r-45cb4.appspot.com",
+    messagingSenderId: "349436375453",
+    appId: "1:349436375453:web:41bef99fbd295c5b833f74",
+    measurementId: "G-D3DEGTQFEP"
+    });
